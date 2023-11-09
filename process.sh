30ec25b38fdcb8814b5680818b6f245f4a80321f
yyyyyyyyuhan <147095674+yyyyyyyyuhan@users.noreply.github.com>
2023-11-09 13:46:50 -0600

    process.sh

--- process.sh
+++ process.sh
@@ -1,6 +1,5 @@
-#!/bin/bash                                                                     
+#!/bin/bash                                                                    
 
-file=largefile0$n
+file=$1
 
-cat $file | tr '[:upper:]' '[:lower:]' | sed 's/[[:punct:]]//g' | sed -e $'s/\t\
-/\\\n/g' -e $'s/ /\\\n/g' | grep -v '^$' | sort > out_${file}.txt
+cat $file | tr '[:upper:]' '[:lower:]' | sed 's/[[:punct:]]//g' | sed -e $'s/\t/\\\n/g' -e $'s/ /\\\n/g' | grep -v '^$' | sort > out_${file}.txt
