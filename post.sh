ccbaa020c038f96a157b424e00b27435364796b8
yyyyyyyyuhan <147095674+yyyyyyyyuhan@users.noreply.github.com>
2023-11-09 14:00:52 -0600

    post.sh

--- post.sh
+++ post.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/bin/bash                                                                                           
+
+
 export LC_ALL=C
-cat out_0*.txt | sort -m | unique -c | sort -rn -o countOfWords.txt
+
+sort -m out_largefile0*.txt | uniq -c | sort -rn -o countOfWords.txt
