b1a2c943ac7b6a28c56fb5e11954d171a2bb57b9
abigailsikora <143763564+abigailsikora@users.noreply.github.com>
2023-11-09 13:46:58 -0600

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LC_ALL=C
 #replace * with how we name the files
-cat * | sort -m | unique -c | sort -rn -o countOfWords.txt
+cat out_0*.txt | sort -m | unique -c | sort -rn -o countOfWords.txt
